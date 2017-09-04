9278b4ffd18eb324c975a131830f4e66227943bb
Amir J <amir.jahan@gmail.com>
2017-09-03 21:50:38 -0700

    Initial Commit

---
+++ MultiPeer_Connection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultiPeer_Connection
+//
+//  Created by Amir on 9/3/17.
+//  Copyright © 2017 Cinard Digital Media Institute. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
